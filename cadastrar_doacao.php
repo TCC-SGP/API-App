2cb41fdd2eaff848faf89436fc551d6969874c0f
Blask23 <blask2307@gmail.com>
2021-12-06 11:46:48 -0300

    Atualizando API

--- cadastrar_doacao.php
+++ cadastrar_doacao.php
@@ -2,21 +2,15 @@
 
 include_once ('conexao.php');
 
-$doador = $_REQUEST['TB_DOADOR_ID'];
-$tipo_doador = $_REQUEST['tipo_doador'];
-$doacao = $_REQUEST['TB_TIPODOACAO_ID'];
-$descricao = $_REQUEST['TB_DOACAO_DESCRICAO'];
-$nomedoador = $_REQUEST['TB_DOACAO_NOMEDOADOR'];
-$quantia = $_REQUEST['TB_DOACAO_QUANTIA'];
+$descricao = $_REQUEST['descricao'];
+$nomedoador = $_REQUEST['nomedoador'];
+$quantia = $_REQUEST['quantia'];
 
-$sql = 'insert into tb_pet (TB_DOADOR_ID, TB_TIPODOACAO_ID, TB_DOACAO_DESCRICAO, TB_DOACAO_NOMEDOADOR, TB_DOACAO_QUANTIA) 
-values (:doador,:tipo_doador,:doacao,:descricao,:nomedoador,:quantia,Now(),Now());';
+$sql = 'insert into tb_doacao (TB_TIPODOACAO_ID, TB_DOACAO_DESCRICAO, TB_DOACAO_NOMEDOADOR, TB_DOACAO_QUANTIA, TB_DOACAO_ESTADO, createdAt, updatedAt)
+values (2,:descricao,:nomedoador,:quantia,"pago",Now(),Now());';
 $stmt = $pdo->prepare($sql);
-$stmt->bindParam(':doador', $protetor, PDO::PARAM_STR);
-$stmt->bindParam(':tipo_doaodr', $tipo_pet, PDO::PARAM_STR);
-$stmt->bindParam(':doacao', $nome, PDO::PARAM_STR);
-$stmt->bindParam(':descricao', $porte, PDO::PARAM_STR);
-$stmt->bindParam(':nomedoador', $raca, PDO::PARAM_STR);
-$stmt->bindParam(':quantia', $idade, PDO::PARAM_STR);
+$stmt->bindParam(':descricao', $descricao, PDO::PARAM_STR);
+$stmt->bindParam(':nomedoador', $nomedoador, PDO::PARAM_STR);
+$stmt->bindParam(':quantia', $quantia, PDO::PARAM_STR);
 $stmt->execute();
 ?>
